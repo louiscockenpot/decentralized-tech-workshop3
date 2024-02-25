9de785244812abd4839cf5e5c759f8f3cb891c95
louiscockenpot <louis.cockenpot@edu.devinci.fr>
2024-02-25 22:08:50 +0100

    Q2 + Q3: Introduced weighting system

--- app/app.py
+++ app/app.py
@@ -1,61 +1,29 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import joblib
 
 app = Flask(__name__)
 
-model = joblib.load('models/model.pkl')
+model = joblib.load('app/models/model.pkl')
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'GET':
-        return render_template('form.html')
-    elif request.method == 'POST':
-        pclass = request.form['pclass']
-        sex = request.form['sex']
-        age = request.form['age']
-        sibsp = request.form['sibsp']
-        parch = request.form['parch']
-        fare = request.form['fare']
-        embarked = request.form['embarked']
-        who = request.form['who']
-        alone = request.form['alone']
     try:
-        pclass = int(pclass)
-        sex = int(sex)
-        age = int(age)
-        sibsp = int(sibsp)
-        parch = int(parch)
-        fare = int(fare)
-        embarked = int(embarked)
-        who = int(who)
-        adult_male = True if who == 1 else False
-        alone = bool(alone)                
-
-        data = {'pclass': [pclass],
-            'sex': [sex],
-            'age': [age],
-            'sibsp': [sibsp],
-            'parch': [parch],
-            'fare': [fare],
-            'embarked': [embarked],
-            'who': [who],
-            'adult_male': [adult_male],
-            'alone': [alone]}
-
-        df = pd.DataFrame(data)
-        scaler = joblib.load('models/scaler_titanic.pkl')
+        data = request.json
+        df = pd.DataFrame([data])
+        scaler = joblib.load('app/models/scaler_titanic.pkl')
         scaled_data = scaler.transform(df)
 
-        prediction = model.predict(scaled_data)
-        return f'Prediction: {"Il aurait survécu" if prediction == 1 else "Il n aurait pas survécu"}'
+        prediction = model.predict(scaled_data)        
+        return jsonify({'prediction': int(prediction[0]), 'model_acc': 0.84}), 200
+
     except ValueError as e:
-        return f'Donnée invalide, veuillez remplir le form a nouveau. Erreur: {e}'
+        return f'Donnée invalide, veuillez remplir le form a nouveau. Erreur: {str(e)}', 400
     
-@app.route('/home')
+@app.route("/")
 def home():
-    return "Welcome to the Titanic survival prediction app"
+    return redirect(url_for("predict"))
 
 
 app.run(host='0.0.0.0', port=8080)
